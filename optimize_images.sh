83b43bcd577bb5a5a0163ac1dc5ee2f166232bd5
Oleksandr Biriukov <100308960+scriptJedi@users.noreply.github.com>
2023-11-17 18:50:38 +0200

    Update optimize_images.sh

--- optimize_images.sh
+++ optimize_images.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-mkdir -p optimize_img/jpg
-mkdir -p optimize_img/webp
+# Определение путей к папкам
+jpg_png_folder="optimize_img/jpg_png"
+webp_folder="optimize_img/webp"
+
+# Создание папок
+mkdir -p "$jpg_png_folder"
+mkdir -p "$webp_folder"
 
 for file in *.*
 do
@@ -11,18 +16,37 @@ do
     fi
 
     filename="${file%.*}"
+    extension="${file##*.}"
+    resolution=$(magick identify -format "%w" "$file")
 
-    magick convert "$file" -resize 600 -quality 50 optimize_img/jpg/"$filename".jpg
+    if [[ "$extension" == "jpg" || "$extension" == "jpeg" ]]; then
+        # Если файл JPEG и разрешение больше 600, то resize, иначе только quality
+        if ((resolution > 600)); then
+            magick convert "$file" -resize 600 -quality 50 "$jpg_png_folder/$filename.jpg"
+        else
+            magick convert "$file" -quality 50 "$jpg_png_folder/$filename.jpg"
+        fi
+    elif [[ "$extension" == "png" ]]; then
+        # Если файл PNG и разрешение больше 600, то resize, иначе только quality
+        if ((resolution > 600)); then
+            magick convert "$file" -resize 600 -quality 50 "$jpg_png_folder/$filename.png"
+        else
+            magick convert "$file" -quality 50 "$jpg_png_folder/$filename.png"
+        fi
+    else
+        # В остальных случаях сохраняем как JPG
+        magick convert "$file" -resize 600 -quality 50 "$jpg_png_folder/$filename.jpg"
+    fi
 
-    magick convert "$file" -resize 600 -quality 50 optimize_img/webp/"$filename"_600px.webp
-    magick convert "$file" -resize 300 -quality 50 optimize_img/webp/"$filename"_300px.webp
+    magick convert "$file" -resize 600 -quality 50 "$webp_folder/$filename"_600px.webp
+    magick convert "$file" -resize 300 -quality 50 "$webp_folder/$filename"_300px.webp
 done
 
 echo '<picture>' > picture.html
 
-echo '  <source media="(max-width: 767.97px)" srcset="optimize_img/webp/01_300px.webp">' >> picture.html
-echo '  <source media="(min-width: 767.98px)" srcset="optimize_img/webp/01_600px.webp">' >> picture.html
-echo '  <img src="optimize_img/jpg/01.jpg" alt="Your Image Description">' >> picture.html
+echo '  <source media="(max-width: 767.97px)" srcset="'$webp_folder'/01_300px.webp">' >> picture.html
+echo '  <source media="(min-width: 767.98px)" srcset="'$webp_folder'/01_600px.webp">' >> picture.html
+echo '  <img src="'$jpg_png_folder'/01.jpg" alt="Your Image Description">' >> picture.html
 
 echo '</picture>' >> picture.html
 
