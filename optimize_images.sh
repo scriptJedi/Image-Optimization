6716adf601317015ecef3bd43791cb1e6e5cfa26
Oleksandr Biriukov <100308960+scriptJedi@users.noreply.github.com>
2023-11-17 18:34:59 +0200

    Update optimize_images.sh

--- optimize_images.sh
+++ optimize_images.sh
@@ -7,7 +7,7 @@ for file in *.*
 do
     filename="${file%.*}"
 
-    magick convert "$file" -resize 50% optimize_img/jpg/"$filename".jpg
+    magick convert "$file" -resize 600 -quality 50 optimize_img/jpg/"$filename".jpg
 
     magick convert "$file" -resize 600 -quality 50 optimize_img/webp/"$filename"_600px.webp
     magick convert "$file" -resize 300 -quality 50 optimize_img/webp/"$filename"_300px.webp
@@ -15,9 +15,9 @@ done
 
 echo '<picture>' > picture.html
 
-echo '  <source media="(max-width: 600px)" srcset="01_300px.webp">' >> picture.html
-echo '  <source media="(min-width: 601px)" srcset="01_600px.webp">' >> picture.html
-echo '  <img src="01.jpg" alt="Your Image Description">' >> picture.html
+echo '  <source media="(max-width: 767.97px)" srcset="optimize_img/webp/01_300px.webp">' >> picture.html
+echo '  <source media="(min-width: 767.98px)" srcset="optimize_img/webp/01_600px.webp">' >> picture.html
+echo '  <img src="optimize_img/jpg/01.jpg" alt="Your Image Description">' >> picture.html
 
 echo '</picture>' >> picture.html
 
