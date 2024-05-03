fee65e71eb584875d3829dcf8a2cba9b89fe9c29
Oleksandr Biriukov <100308960+scriptJedi@users.noreply.github.com>
2024-05-03 14:42:27 +0300

    Update optimize_images.sh

--- optimize_images.sh
+++ optimize_images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Определение путей к папкам
-jpg_png_folder="imgs/jpg_png"
-webp_folder="imgs/webp"
+jpg_folder="images/jpg"
+webp_folder="images/webp"
 
 # Создание папок
-mkdir -p "$jpg_png_folder"
+mkdir -p "$jpg_folder"
 mkdir -p "$webp_folder"
 
 # Общие параметры обработки изображений
@@ -15,10 +15,9 @@ resize_small=600
 
 echo '<picture>' > picture.html
 
-for file in *.*; 
-do
+for file in *.*; do
     # Исключаем файлы с расширением svg
-    if [[ "$file" == *.svg || ( "$file" != *.jpg && "$file" != *.jpeg && "$file" != *.png ) ]]; then
+    if [[ "$file" == *.svg || !("$file" =~ \.(jpg|jpeg|png)$) ]]; then
         continue
     fi
 
@@ -26,35 +25,24 @@ do
     extension="${file##*.}"
     resolution=$(magick identify -format "%w" "$file")
 
-    if [[ "$extension" == "jpg" || "$extension" == "jpeg" ]]; then
-        # Если файл JPEG и разрешение больше 1200, то resize, иначе только quality
-        if ((resolution > 1200)); then
-            magick convert "$file" -resize $resize_large -quality $quality "$jpg_png_folder/$filename.jpg"
-        else
-            magick convert "$file" -quality $quality "$jpg_png_folder/$filename.jpg"
-        fi
-    elif [[ "$extension" == "png" ]]; then
-        # Если файл PNG и разрешение больше 1200, то resize, иначе только quality
-        if ((resolution > 1200)); then
-            magick convert "$file" -resize $resize_large -quality $quality "$jpg_png_folder/$filename.png"
-        else
-            magick convert "$file" -quality $quality "$jpg_png_folder/$filename.png"
-        fi
-    else
-        # В остальных случаях сохраняем как JPG
-        magick convert "$file" -resize $resize_large -quality $quality "$jpg_png_folder/$filename.jpg"
+    # Преобразование PNG и JPEG в JPG
+    if [[ "$extension" == "png" || "$extension" == "jpeg" || "$extension" == "jpg" ]]; then
+        magick convert "$file" -background white -alpha remove -resize $resize_large -quality $quality "$jpg_folder/$filename.jpg"
     fi
 
+    # Создание WebP для изображений
     magick convert "$file" -resize $resize_large -quality $quality "$webp_folder/$filename"_1200px.webp
     magick convert "$file" -resize $resize_small -quality $quality "$webp_folder/$filename"_600px.webp
 done
 
+# Добавление элементов <source> и <img> в HTML
 echo '  <source media="(max-width: 767.97px)" srcset="'$webp_folder'/01_600px.webp">' >> picture.html
 echo '  <source media="(min-width: 767.98px)" srcset="'$webp_folder'/01_1200px.webp">' >> picture.html
-echo '  <img src="'$jpg_png_folder'/01.jpg" alt="Your Image Description">' >> picture.html
+echo '  <img src="'$jpg_folder'/01.jpg" alt="Your Image Description">' >> picture.html
 
 echo '</picture>' >> picture.html
 
+# Вывод текстового арта
 echo -e '\033[1;32m████─████─█───█─████─█───███─███─███\033[0m'
 echo -e '\033[1;32m█──█─█──█─██─██─█──█─█───█────█──█──\033[0m'
 echo -e '\033[1;32m█────█──█─█─█─█─████─█───███──█──███\033[0m'
